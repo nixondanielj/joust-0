e3d46ff4603059a41fa94e998014d0e3db22ce9b
Dan Nixon <dan.nixon@onshoreoutsourcing.com>
2016-11-01 15:47:49 -0400

    added my test

--- test/DotNetCore.Test/Joust_AddShould.cs
+++ test/DotNetCore.Test/Joust_AddShould.cs
@@ -18,5 +18,12 @@ namespace DotNetCore.Test
             int result = solution.Add(2, 2);
             Assert.Equal(result, 4);
         }
+
+        [Fact]
+        public void ReturnFortyAddingTenAndThirty()
+        {
+            int result = solution.Add(10, 30);
+            Assert.Equal(result, 40);
+        }
     }
 }
