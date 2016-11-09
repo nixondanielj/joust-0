fdef72d819cf85dc2c0dab26478a14e2e0b70493
Dan Nixon <nixon.daniel.j@gmail.com>
2016-11-09 16:15:53 -0500

    added tests, fixed name

---
+++ test/DotNetCore.Test/Joust_IsLinkValidShould.cs
@@ -0,0 +1,58 @@
+﻿using DotNetCore.Joust;
+using Xunit;
+
+namespace DotNetCore.Test
+{
+    public class Joust_IsLinkValidShould
+    {
+        private readonly IJoust solution;
+
+        public Joust_IsLinkValidShould()
+        {
+            solution = null;
+        }
+
+        [Fact]
+        public void ReturnTrueForOnshore()
+        {
+            bool result = solution.IsLinkValid("http://www.onshoreoutsourcing.com");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ReturnFalseForBadFormat()
+        {
+            bool result = solution.IsLinkValid("orange");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ReturnFalseForNull()
+        {
+            bool result = solution.IsLinkValid(null);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ReturnFalseForNotFoundUrl()
+        {
+            // response should be 404
+            bool result = solution.IsLinkValid("http://www.onshoreoutsourcing.com/foo");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ReturnFalseForNonexistentUrl()
+        {
+            bool result = solution.IsLinkValid("http://deadurl.fake");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ReturnTrueForRedirect()
+        {
+            bool result = solution.IsLinkValid("http://onshoreoutsourcing.com");
+            Assert.True(result);
+        }
+    }
+}
